2e987bc057d96f2bf47fa44c179c24d99fdf6ddd
Pratik Patil <pppratikpatil723@gmail.com>
2018-07-06 20:07:14 +0530

    Updated Function Pointer to member Function

---
+++ Concepts_Code/ptr_Function_Pointer/func_ptr_member_func.cpp
@@ -0,0 +1,51 @@
+/* ****************************** Prototype ********************************************
+
+    Declaration :   return_type(class :: *ptr_Name)(args) = &class :: FunctionName;
+    Invoke      :   (ObjName.*ptr_Name)(args);
+
+****************************************************************************************/
+#include<iostream>
+using namespace std;
+
+class Explore;
+void (Explore :: *Fptr1)(void) = NULL;
+void (Explore :: *Fptr2)(void) = NULL;
+
+class Explore
+{
+    private :
+        void Function1()
+        {
+            cout << "Calling Private Function1" << endl;
+        }
+
+    public :
+        void Function2()
+        {
+            cout << "Calling Public Function2" << endl;
+        }
+
+        void Assignment()
+        {
+            Fptr1 = &Explore :: Function1;
+            Fptr2 = &Explore :: Function2;
+        }
+};
+
+int main()
+{
+    Explore ObjExplore;
+    Explore ObjExplore1;
+    Explore *ptrExplore = &ObjExplore1;
+
+    ObjExplore.Assignment();
+    (ObjExplore.*Fptr1)();  // Private Functions can also be called outside class using
+                            // Function pointer to member function.
+    (ObjExplore.*Fptr2)();
+
+//   ptrExplore -> Assignment();      Fails in case of using pointer instead of object.
+//  (ptrExplore -> *Fptr1)();
+//  (ptrExplore -> *Fptr2)();
+
+    return 0;
+}
