2dd7df35b01acaf7fb8b004360223120b8b45a6d
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2018-08-26 20:51:10 +0530

    Update

--- OOPS/Storage_Classes/1.auto_register_extern.cpp
+++ OOPS/Storage_Classes/1.auto_register_extern.cpp
@@ -2,9 +2,15 @@
 
 int main ()
 {
+    auto int var1 = 10;     //OR int var1 = 10;
     StorageHelper ObjStorageHelper;
     return 0;
 }
+
+// auto is automatic storage class which is default storage class of variables.
+// In C++ 11 there as been improvement for automatic type deduction. Variables can be initiliaze
+// using only auto storagr class. i,e. auto var1 = 10; which will atomatic consider var1 as
+// integer type.
 
 // Compile with "g++ .\storagehelper.cpp .\1.auto_register_extern.cpp"
 // Compile with "g++ storagehelper.cpp 1.auto_register_extern.cpp"
