e9f36f0b857885396f0275c057f4432f8b2980c0
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2018-08-28 17:48:18 +0530

    Update

--- OOPS/Storage_Classes/1.auto_register_extern.cpp
+++ OOPS/Storage_Classes/1.auto_register_extern.cpp
@@ -1,9 +1,12 @@
 #include "storagehelper.h"
+int Variable = 100;
 
 int main ()
 {
-    auto int var1 = 10;     //OR int var1 = 10;
+    auto int var1 = 10;     //OR int var1 = 10;     // OR auto vatr1 = 10;
+    register int var2 = 20;
     StorageHelper ObjStorageHelper;
+    extern int Variable;
     return 0;
 }
 
@@ -11,6 +14,12 @@ int main ()
 // In C++ 11 there as been improvement for automatic type deduction. Variables can be initiliaze
 // using only auto storagr class. i,e. auto var1 = 10; which will atomatic consider var1 as
 // integer type.
+
+// register storage class declares register variables which have the same functionality as that of the auto
+// variables. The only difference is that the compiler tries to store these variables in the register of the
+// microprocessor if a free register is available. This makes the use of register variables to be much faster
+// than that of the variables stored in the memory during the runtime of the program. If a free register is
+// not available, these are then stored in the memory only.
 
 // Compile with "g++ .\storagehelper.cpp .\1.auto_register_extern.cpp"
 // Compile with "g++ storagehelper.cpp 1.auto_register_extern.cpp"
--- OOPS/Storage_Classes/storagehelper.h
+++ OOPS/Storage_Classes/storagehelper.h
@@ -1,13 +1,14 @@
 #ifndef STORAGEHELPER_H
 #define STORAGEHELPER_H
 #include<iostream>
-#include <pthread.h>
+#include<pthread.h>
 using namespace std;
 
 class StorageHelper
 {
 public:
     StorageHelper();
+
 };
 
 #endif // STORAGEHELPER_H
