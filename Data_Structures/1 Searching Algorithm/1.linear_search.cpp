fc395cff7f404d3b5110427228cbb348f35fb72f
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2018-09-16 23:11:11 +0530

    Added Searching Algorithm and Update

---
+++ Data_Structures/1 Searching Algorithm/1.linear_search.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+# define MAX_SIZE 10
+int linearSearch(int arr[],int data)
+{
+    int i=0;
+    for (i=0; i<MAX_SIZE; i++){if(data == arr[i]){return i;}}
+    return -1;
+}
+
+int main ()
+{
+    int data,index,arr[MAX_SIZE] = {1,2,3,4,5,6,7,8,9,10};
+    cout << "Please enter the no. to be found : ";
+    cin >> data;
+
+    index = linearSearch(arr,data);
+
+    if(index != -1) {cout << "Entered No. found at Location " << index+1 << endl;}
+    else {cout << "Entered No. not Found" << endl;
+    }
+    return 0;
+}
