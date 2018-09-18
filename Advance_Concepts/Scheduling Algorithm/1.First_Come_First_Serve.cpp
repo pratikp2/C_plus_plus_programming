eca50b4a5ab6d6eb8be7bdefae03466239da3dee
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2018-09-18 23:25:33 +0530

    Updated Priority Scheduling

--- Advance_Concepts/Scheduling Algorithm/1.First_Come_First_Serve.cpp
+++ Advance_Concepts/Scheduling Algorithm/1.First_Come_First_Serve.cpp
@@ -23,16 +23,54 @@ using namespace std;
 
 # define NumberOfProcesses 4
 
+int AvgWaitTime = 0;
+int AvgTurnAroundTime = 0;
 int Waiting_Time[NumberOfProcesses];
-int Burst_Time[NumberOfProcesses];
 int TurnAround_Time[NumberOfProcesses];
-int Processes[NumberOfProcesses];
+
+void findWaitingTime(int Burst_Time[])
+{
+    Waiting_Time[0] = 0;
+    for (int i=1; i<NumberOfProcesses; i++)
+    {
+        Waiting_Time[i] = Waiting_Time[i-1] + Burst_Time[i-1];
+    }
+}
+
+void findTurnAroundTime(int Burst_Time[])
+{
+    for (int i=0; i<NumberOfProcesses; i++)
+    {
+        TurnAround_Time[i] = Waiting_Time[i] + Burst_Time[i];
+    }
+}
+
+void findAverageTime()
+{
+    for (int i=0; i<NumberOfProcesses; i++)
+    {
+        AvgWaitTime = AvgWaitTime + Waiting_Time[i];
+        AvgTurnAroundTime = AvgTurnAroundTime + TurnAround_Time[i];
+    }
+}
 
 int main()
 {
-    Processes = {1,2,3,4};
-    Burst_Time = {21,3,6,3};
+    int Processes[NumberOfProcesses] = {1,2,3,4};
+    int Burst_Time[NumberOfProcesses] = {21,3,6,2};
+
+    findWaitingTime(Burst_Time);
+    findTurnAroundTime(Burst_Time);
+    findAverageTime();
+
+    cout << "Process    " << "Burst Time    " << "Waiting Time      " << "Turn Around Time" <<endl <<endl;
+    for(int i=0; i<NumberOfProcesses; i++)
+    {
+        cout << "   " <<1+i << "\t\t" << Burst_Time[i] << "\t\t" << Waiting_Time[i] << "\t\t" << TurnAround_Time[i] << endl;
+    }
+
+    cout <<"Average Waiting Time is     : "<< (float)AvgWaitTime/(float)NumberOfProcesses <<endl;
+    cout <<"Average Turn around Time is : "<< (float)AvgTurnAroundTime/(float)NumberOfProcesses <<endl<<endl;
+
     return 0;
 }
-
-// https://www.studytonight.com/operating-system/cpu-scheduling
---
+++ Advance_Concepts/Scheduling Algorithm/3.Priority_Scheduling.cpp
@@ -0,0 +1,85 @@
+/** **********************************************************************************************************************
+ *
+ *  1. Priority is assigned for each process.
+ *  2. Process with highest priority is executed first and so on.
+ *  3. Processes with same priority are executed in FCFS manner.
+ *  4. Priority can be decided based on memory requirements, time requirements or any other resource requirement.
+ *
+**************************************************************************************************************************/
+
+# include <iostream>
+using namespace std;
+
+# define NumberOfProcesses 4
+
+int AvgWaitTime = 0;
+int AvgTurnAroundTime = 0;
+int Waiting_Time[NumberOfProcesses];
+int TurnAround_Time[NumberOfProcesses];
+int Processed_Burst_Time[NumberOfProcesses];
+
+void processPriority(int Priority[],int Burst_Time[])
+{
+    for(int i=0; i<NumberOfProcesses; i++)
+    {
+        for(int j=0; j<NumberOfProcesses; j++)
+        {
+            if(i+1 == Priority[j])
+            {
+                Processed_Burst_Time[i] = Burst_Time[j];
+            }
+        }
+        cout <<endl;
+    }
+}
+
+void findWaitingTime()
+{
+    Waiting_Time[0] = 0;
+    for (int i=1; i<NumberOfProcesses; i++)
+    {
+        Waiting_Time[i] = Waiting_Time[i-1] + Processed_Burst_Time[i-1];
+    }
+}
+
+void findTurnAroundTime()
+{
+    for (int i=0; i<NumberOfProcesses; i++)
+    {
+        TurnAround_Time[i] = Waiting_Time[i] + Processed_Burst_Time[i];
+    }
+}
+
+void findAverageTime()
+{
+    for (int i=0; i<NumberOfProcesses; i++)
+    {
+        AvgWaitTime = AvgWaitTime + Waiting_Time[i];
+        AvgTurnAroundTime = AvgTurnAroundTime + TurnAround_Time[i];
+    }
+}
+
+int main()
+{
+    int Processes[NumberOfProcesses] = {1,2,3,4};
+    int Burst_Time[NumberOfProcesses] = {21,3,6,2};
+    int Priority[NumberOfProcesses] = {2,1,4,3};
+
+    processPriority(Priority,Burst_Time);
+    findWaitingTime();
+    findTurnAroundTime();
+    findAverageTime();
+
+    cout << "Process    "<< "Priority   "<< "Burst Time    " << "Processed Burst Time      "<< "Waiting Time      " << "Turn Around Time" <<endl <<endl;
+    for(int i=0; i<NumberOfProcesses; i++)
+    {
+        cout << "   " <<1+i << "\t      " <<Priority[i] << "\t          "<< Burst_Time[i] << "\t\t  "<< Processed_Burst_Time[i] << "\t\t\t  " << Waiting_Time[i] << "\t\t  " << TurnAround_Time[i] << endl;
+    }
+
+    cout <<"Average Waiting Time is     : "<< (float)AvgWaitTime/(float)NumberOfProcesses <<endl;
+    cout <<"Average Turn around Time is : "<< (float)AvgTurnAroundTime/(float)NumberOfProcesses <<endl<<endl;
+
+    return 0;
+}
+
+// https://www.studytonight.com/operating-system/introduction-operating-systems
