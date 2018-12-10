9a20ceafd4b659e9750f5b82b6204bf4c9c98891
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2018-12-10 11:02:52 +0530

    Updated Endianness Comments

--- Concepts_Code/Endianess_of_Machine/main.cpp
+++ Concepts_Code/Endianess_of_Machine/main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -11,3 +12,14 @@ int main()
 
     return 0;
 }
+
+// Little Endian : The Most Significatn byte is stored at Last of byte of the allocated Memory. i,e. at higher address.
+// eg for a hexadecomal No 0X12345678.
+//      Address     00      01      02      03
+//      Digits      78      56      34      12
+
+// Big Endian : On other Hand for big Endian Most Significatn byte is stored at first of byte of the allocated Memory.
+// i,e. at lower address.
+// eg for a hexadecomal No 0X12345678.
+//      Address     00      01      02      03
+//      Digits      12      34      56      78
