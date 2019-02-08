82ffd8d87aaad1d1b0a0d49a949dad0fa8928b26
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2019-02-08 18:20:45 +0530

    Certification Update

--- Advance_Concepts/File_Handeling_And_Data_Serilization/Boost_Serialization/Boost_Serialization/main.cpp
+++ Advance_Concepts/File_Handeling_And_Data_Serilization/Boost_Serialization/Boost_Serialization/main.cpp
@@ -131,3 +131,32 @@ int main(int argc, char** argv)
 // restored because the old version did not have such a member variable.
 
 // ##############################################################################################################################################
+
+/*#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include "json/json.h"
+int main(void) {
+	Json::Reader reader;
+	Json::Value root;
+	Json::StyledStreamWriter writer;
+	std::string text = "{ \"first\": \"James\", \"last\": \"Bond\", \"nums\": [0, 0, 7] }";
+	std::ofstream outFile;
+	// Parse JSON and print errors if needed
+	if (!reader.parse(text, root)) {
+		std::cout << reader.getFormattedErrorMessages();
+		exit(1);
+	}
+	else {
+		// Read and modify the json data
+		std::cout << "Size: " << root.size() << std::endl;
+		std::cout << "Contains nums? " << root.isMember("nums") << std::endl;
+		root["first"] = "Jimmy";
+		root["middle"] = "Danger";
+		// Write the output to a file
+		outFile.open("output.json");
+		writer.write(outFile, root);
+		outFile.close();
+	}
+	return 0;
+}*/
