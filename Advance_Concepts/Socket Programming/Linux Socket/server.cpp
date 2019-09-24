90f7fd9be6818b702d27cd2e308965623e67af2f
Pratik Patil <pppratipatil723@gmail.com>
2019-09-24 19:41:29 +0530

    Update

--- Advance_Concepts/Socket Programming/Linux Socket/server.cpp
+++ Advance_Concepts/Socket Programming/Linux Socket/server.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
+#include <sys/socket.h>  
+#include <netinet/in.h> 
+#include <string> 
 
+#include <unistd.h>
+#include <stdlib.h> 
+
+#define PORT 8080
+
+
+int main()
+{
+
+ 	std::cout << "HELLO" << std::endl;
+	return 0;
+}
