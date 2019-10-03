661093d23ec4d40633e2fa28a91126d24f875b03
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2019-10-03 23:00:08 +0530

    Updated C++ 11 Features

---
+++ C++_11/4_Class_Features.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Explore
+{
+private :
+    int A = 10,a;                   //
+    const string Hold = "Hello";      // Allows defination inside class body.
+    float fraction = 2.5f, frac;    //
+
+public :
+    Explore(int a, float frac)
+    {
+        this->a = a;
+        this->frac = frac;
+    }
+
+    Explore():Explore(25,5.5f)      // Constructor Deligation : Attribues can be instantiated by calling other
+    {                               // contructors.
+
+    }
+
+    void printVal()
+    {
+        cout << "A      : " << A << endl;
+        cout << "a      : " << a << endl;
+        cout << "Hold   : " << Hold << endl;
+        cout << "Float1 : " << fraction << endl;
+        cout << "Float2 : " << frac << endl;
+    }
+};
+
+int main()
+{
+    Explore objExplore;
+    objExplore.printVal();
+    return 0;
+}
---
+++ C++_11/5_Additional_Keywords.cpp
@@ -0,0 +1,42 @@
+# include <iostream>
+using namespace std;
+
+class Explore final { };                    // <--
+
+class Base
+{
+public:
+    virtual void Function1() {}
+    Base(const Base &objBase) = delete{ }   // <--
+    Base * ptrBase = nullptr;               // <--
+};
+
+class Derived
+{
+public:
+    Derived() default { }                   // <--
+    void Function1() override {}            // <--
+}
+
+int main()
+{
+   return 0;
+}
+
+// final    : If keyword "final" is mentioned in front of a class, then Compiler will not allow any other class
+//            to inherit from that Class .
+
+// delete   : The compiler prohibits the usage of a deleted function.
+
+// nullptr  : nullptr is a keyword that can be used at all places where NULL is expected. Like NULL, nullptr is
+//            implicitly convertible and comparable to any pointer type. Unlike NULL, it is not implicitly
+//            convertible or comparable to integral types.
+
+// default  : If no user-declared constructors of any kind are provided for a class type (struct, class, or union),
+//            the compiler will always declare a default constructor as an inline public member of its class.
+
+// override : 1. Ensures there is a method with the same name in the parent class.
+//            2. The method in the parent class is declared as “virtual” which means it was intended to be rewritten.
+//            3. The method in the parent class has the same signature as the method in the subclass.
+
+
