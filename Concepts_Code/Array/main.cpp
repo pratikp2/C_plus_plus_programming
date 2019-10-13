b1a9f9d917ad7ef48b3d1e233c46213fa2b66767
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2019-10-13 22:00:33 +0530

    Updated Array Code

--- Concepts_Code/Array/main.cpp
+++ Concepts_Code/Array/main.cpp
@@ -1,37 +1,73 @@
 #include <iostream>
 using namespace std;
 
-int* add(int a[],int b[],int n);
+const int size1D = 8;
+const int size2D = 4;
+const int size3D = 2;
 
+
+
+void show1DArray(int []);
+void show2DArray(int [size2D][size2D]);              // const size is needed while passing Multidimentional arrays
+void show3DArray(int [size3D][size3D][size3D]);      // array as an argument
 
 int main()
 {
- int arrSize=8;
 
- int arr[]={1,2,3,4,5,6,7,8};
- int arr1[]={1,2,3,4,5,6,7,8};
- int total[8]={0};
- int *addr= add(arr,arr1,arrSize);
+    int arr1D[]={1,2,3,4,5,6,7,8};
+    show1DArray(arr1D);
 
+    cout << endl << endl;
 
- for(int i=0;i<8;i++)
-    {
-     total[i]=*addr;
-     cout<<total[i]<<endl;
-     (++addr);
-    }
+    int arr2D[size2D][size2D] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    show2DArray(arr2D);
+
+    cout << endl << endl;
+
+    int arr3D[size3D][size3D][size3D] = {0,1,2,3,4,5,6,7};
+    show3DArray(arr3D);
+
     return 0;
 }
 
-
-
-int* add(int a[],int b[],int n)
+void show1DArray(int arr[size1D])
 {
-    for(int i=0;i<n;i++)
+    cout << "Printing 1D Array : " << endl;
+    for(int i=0; i<size1D; i++)
     {
-       a[i]=b[i]+a[i];
+        cout << arr[i] << " ";
     }
-    return a;
+    cout << endl;
+}
+
+void show2DArray(int arr[size2D][size2D])
+{
+    cout << "Printing 2D Array : " << endl;
+    for(int i=0; i<size2D; i++)
+    {
+        for(int j=0; j<size2D; j++)
+        {
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+void show3DArray(int arr[size3D][size3D][size3D])
+{
+    cout << "Printing 3D Array : " << endl;
+    for(int i=0; i<size3D; i++)
+    {
+        for(int j=0; j<size3D; j++)
+        {
+            for(int k=0; k<size3D; k++)
+            {
+                cout << arr[i][j][k] << "  ";
+            }
+            cout << endl;
+        }
+        cout << endl << endl;
+    }
 }
 
 
