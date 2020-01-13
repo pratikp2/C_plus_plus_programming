94386f334600e489a90b48650b28c0d4da6bde92
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2020-01-13 18:19:16 +0530

    Updated Leet Code

--- Leet_Code_Puzzles/3.Longest_Substring_Without_repeating _characters.cpp
+++ Leet_Code_Puzzles/3.Longest_Substring_Without_repeating _characters.cpp
@@ -1,1 +1,45 @@
-main ()
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<char> vec;
+vector<char> :: iterator itr;
+
+int lengthOfLongestSubstring(string s)
+{
+    int temp = 0;
+    int temp1 = 0;
+    int size = s.length();
+    char holder[size+1];
+    strcpy(holder, s.c_str());
+
+    for(int i=0; i<(size-temp); i++)
+    {
+        for (int j=i; j<size; j++)
+        {
+            if(std::find (vec.begin(), vec.end(), holder[i]) == vec.end())
+            {
+                vec.push_back(holder[i]);
+                cout << " Holder " << holder[i] << " Vector " << *--(vec.end()) << endl;
+                temp1++;
+            }
+        }
+        temp = (temp>temp1) ? temp : temp1;
+        cout << " Temp " << temp <<endl;
+    }
+    return temp;
+}
+
+int main()
+{
+    string s;
+    //s = "abcabcab";
+    //s = "bbbbb";
+    //s = "pwwkew";
+    s = "au";
+    cout << "Longest substring no is : " << lengthOfLongestSubstring(s) << endl;
+    return 0;
+}
