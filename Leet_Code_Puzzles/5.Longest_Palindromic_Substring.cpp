30d0f2e77836317bb616edfe13e4283edb4ecc20
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2020-01-14 22:55:46 +0530

    Updated Longest substring in Palindrome

--- Leet_Code_Puzzles/5.Longest_Palindromic_Substring.cpp
+++ Leet_Code_Puzzles/5.Longest_Palindromic_Substring.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstring>
+#include <string>
 using namespace std;
+
+string longestPalindrome(string s)
+{
+    string palindrome = "";
+    string dummy = "";
+
+    int size = s.length();
+    char holder[size+1];
+    strcpy(holder, s.c_str());
+
+    for(int i=0; i<(size-1); i++)
+    {
+        for(int j=i+1; j<size; j++)
+        {
+
+        }
+        palindrome = (palindrome.length() > dummy.length()) ? palindrome : dummy;
+        dummy = "";
+    }
+    return palindrome;
+}
 
 int main()
 {
+    string s = "babad";
+    cout << "Longest Substring Palindrome length is : " << longestPalindrome(s) << endl;
     return 0;
 }
