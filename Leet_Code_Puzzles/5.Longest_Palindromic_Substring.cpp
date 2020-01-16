42c2d9e7e7f7568f980b2e71a73020eb6ae8a0cf
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2020-01-16 17:33:17 +0530

    Updated Longest substring palindrome

--- Leet_Code_Puzzles/5.Longest_Palindromic_Substring.cpp
+++ Leet_Code_Puzzles/5.Longest_Palindromic_Substring.cpp
@@ -5,10 +5,48 @@ using namespace std;
 
 string longestPalindrome(string s)
 {
+    int size = s.length();
     string palindrome = "";
     string dummy = "";
+    bool flag = false;
+    char holder[size+1];
+    strcpy(holder, s.c_str());
 
+    for(int i=0; i<(size-1); i++)
+    {
+        for(int j=size; j>=i+1; j--)
+        {
+            if(holder[i] == holder[j])
+            {
+
+            }
+        }
+    }
+    if (palindrome == "")
+        return palindrome = holder[0];
+    return palindrome;
+}
+
+int main()
+{
+    string s;
+    //s = "babad";
+    //s = "badefbey";
+    //s = "cbbd";
+    //s = "ccc";
+    //s = "caba";
+    s = "abababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababababa";
+    cout << "Longest Substring Palindrome length is : " << longestPalindrome(s) << endl;
+    return 0;
+}
+
+
+/**string longestPalindrome(string s)
+{
     int size = s.length();
+    string palindrome = "";
+    string dummy = "";
+    bool flag = false;
     char holder[size+1];
     strcpy(holder, s.c_str());
 
@@ -16,17 +54,37 @@ string longestPalindrome(string s)
     {
         for(int j=i+1; j<size; j++)
         {
-
+            if(holder[i] == holder[j])
+            {
+                //cout << "index " << i << " = " << holder[i] <<" index " << j << " = " << holder[j] << endl;
+                int p=i,q=0;
+                for (int k=i; k<=(i+(j-i)/2); k++)
+                {
+                    cout << "Size " << i+(j-i)/2 << endl;
+                    cout << "index " << k << " = " << holder[k] <<" index " << j-k << " = " << holder[j-k] << endl;
+                    if(holder[k] != holder[j-q])
+                    {
+                        flag = false;
+                        break;
+                    }
+                    else
+                        flag = true;
+                    q++;
+                }
+                while(p <= j && flag == true)
+                {
+                    dummy = dummy + holder[p];
+                    p++;
+                }
+                if(flag)
+                    palindrome = (palindrome.length() > dummy.length()) ? palindrome : dummy;
+                cout << dummy << endl;
+                dummy = "";
+                flag = false;
+            }
         }
-        palindrome = (palindrome.length() > dummy.length()) ? palindrome : dummy;
-        dummy = "";
     }
+    if (palindrome == "")
+        return palindrome = holder[0];
     return palindrome;
-}
-
-int main()
-{
-    string s = "babad";
-    cout << "Longest Substring Palindrome length is : " << longestPalindrome(s) << endl;
-    return 0;
-}
+}*/
