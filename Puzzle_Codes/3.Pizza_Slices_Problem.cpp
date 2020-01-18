8047f90c1baf03e8f5a07723e7330af242aec584
Pratik Patil <37364964+pratikp2@users.noreply.github.com>
2020-01-18 22:18:56 +0530

    Google Hash Code - Established means for input and output file

--- Puzzle_Codes/3.Pizza_Slices_Problem.cpp
+++ Puzzle_Codes/3.Pizza_Slices_Problem.cpp
@@ -1,62 +1,92 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<cstdlib>
-#include<vector>
-#include<cstring>
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <map>
+
 using namespace std;
 
-void ExtractData(string s,vector<char> &data)
+struct DataTemplate
 {
-    int size = s.length();
-    char holder[size+1];
-    strcpy(holder, s.c_str());
+    int MaxSlices;
+    int NoOfPizzas;
+    map<int,int> slices;
+};
 
-    for(int i=0; i<size; i++)
-    {
-        data.push_back(holder[i]);
-    }
-}
-
-void ConvertData(vector<int> &data, vector<char> &stringdata)
-{
-    int i = stringdata.size();
-    string s = "";
-    while(true)
-    {
-        while(stringdata[i] != ' ')
-        {
-            s=s+stringdata[i];
-            i--;
-        }
-        if(i==0)
-            break;
-    }
-    //data.push_back(atoi(stringdata[i]));
-}
+struct DataTemplate* ConvertData(string);
+string GetString();
+void SetString(struct DataTemplate*);
 
 int main()
 {
-    string TEMP;
-    ifstream DATA_FILE;
-    vector<int> data;
-    vector<char> stringdata;
-    vector<char> :: iterator itr;
+    DataTemplate * ptr;
+    string s = GetString();
+    ptr = ConvertData(s);
 
-    try
-    {
-        DATA_FILE.open("a_example.in",ios::in);
-        getline(DATA_FILE,TEMP);
-        ExtractData(TEMP, stringdata);
-        getline(DATA_FILE,TEMP);
-        ExtractData(TEMP, stringdata);
-        DATA_FILE.close();
-    }
-    catch (exception x) { cout << "File Error ... !" << endl;}
-
-    //for (itr = stringdata.begin(); itr != stringdata.end(); ++itr){ cout << *itr << " ";}
-    //cout << endl;
+    cout << "Input Str  :   " << s << endl << endl;
+    cout << "Maxium No  :   " << ptr->MaxSlices << endl;
+    cout << "Pizza No   :   " << ptr->NoOfPizzas << endl;
+    cout << "Sequence   :   ";
+    for(int i=0; i<ptr->slices.size(); i++)
+        cout << ptr->slices[i] << " ";
+    SetString(ptr);
 
     return 0;
 }
 
+struct DataTemplate* ConvertData(string s)
+{
+    DataTemplate * ptr = new DataTemplate;
+
+    string temp1(s,0,s.find(" "));
+    ptr->MaxSlices = stoi(temp1);
+    s.erase(0,s.find(" ")+1);
+
+    string temp2 (s,0,s.find(" "));
+    ptr->NoOfPizzas = stoi(temp2);
+    s.erase(0,s.find(" ")+1);
+
+    for(int i=0; i<ptr->NoOfPizzas; i++)
+    {
+        string temp(s,0,s.find(" "));
+        s.erase(0,s.find(" ")+1);
+        ptr->slices.insert(pair<int,int> (i,stoi(temp)));
+    }
+    return ptr;
+}
+
+string GetString()
+{
+    string s1,s2;
+    ifstream DATA_FILE;
+
+    try
+    {
+        DATA_FILE.open("b_small.in",ios::in);
+        getline(DATA_FILE,s1);
+        getline(DATA_FILE,s2);
+        s1 = s1 + " " + s2;
+        DATA_FILE.close();
+    }
+    catch (exception x) { cout << "File Error ... !" << endl;}
+    return s1;
+}
+
+void SetString(struct DataTemplate * ptr)
+{
+    int size = ptr->slices.size();
+    string temp = "";
+    try
+    {
+        ofstream DATA_FILE;
+        DATA_FILE.open("Ans.out",ios::out);
+
+        DATA_FILE << size <<endl;
+        for(int i=0; i<size; i++)
+            temp = temp + " " + to_string(ptr->slices[i]);
+        temp.erase(0,1);
+         DATA_FILE << temp <<endl;
+
+        DATA_FILE.close();
+    }
+    catch (exception x){ cout << "File Error ... !" << endl;}
+}
