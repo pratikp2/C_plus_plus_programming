303d98c7468f7a7796b49b777f823e61771260a4
Pratik Patil <pppratikpatil723@gmail.com>
2024-06-02 16:19:04 +0200

    DSA restructure and functions update

---
+++ Data Structures and Algorithms/Handy Functions/main.cpp
@@ -0,0 +1,77 @@
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <numeric> //For accumulate operation
+
+int main() {
+    
+    //Vector
+    std::vector<int> vec{100, 200, 300, 400, 500};
+    std::cout << vec.front();               // get 1st element
+    std::cout << vec.back();                // get last element
+    vec.push_back(100);                     // insert an element at last
+    vec.pop_back();                         // remove the last element
+
+        /**************************************************************************************/
+
+    //Map
+    std::map <int, int> Num;
+    Num.insert(std::pair <int, int>(0,1));          // Insert in map
+    Num.find(0);                                    // Finds given key and returns the itr
+
+        /**************************************************************************************/
+
+    //STL Algorithms
+    std::count(vec.begin(), vec.end(), 2);                              // count the number of instances given data appears in th given range
+    std::sort(vec.begin(), vec.end());                                  // to sort a container in ascending order
+    std::sort(vec.begin(), vec.end(), std::greater<int>());             // to sort a container in descending order
+    std::reverse(vec.begin(), vec.end());                               // to reverse a container values
+    std::accumulate(vec.begin(), vec.end(), 0);                         // Calculates the sum of fiven range
+    std::max_element(vec.begin(), vec.end());                           // to find max element within container
+    std::min_element(vec.begin(), vec.end());                           // to find min element within container
+
+    std::string sample = "qwertyuiop";
+    std::sort(sample.begin(), sample.end());
+	do {
+		std::cout << sample << '\n';
+	} while (std::next_permutation(sample.begin(), sample.end()));      // to check all the permutation of a container
+
+        /**************************************************************************************/
+
+    // String Manupulations
+    std::to_string(12345);                      // Convert given number in string
+    std::stoi("12345");                         // convert a string in to number
+
+    isupper('a');                               // Check if char is upper Case A-Z.
+    toupper('a');                               // Convert char to upper case.
+    islower('A');                               // Check if char is lower Case a-z.
+    tolower('Z');                               // Convert char to lower case.
+
+    isalnum('.');                               // Check for Alphanumeric A-Z, a-z, 0-9
+    isalpha('a');                               // Check for Alphanumeric A-Z and a-z
+    isdigit('6');                               // Check for Alphanumeric 0-9
+    ispunct('!');                               // characters that are neither alphanumeric nor a space
+                                                // ! " # $ % & ' ( ) * +, - . / : ;  ? @ [ \ ] ^ _ ` { | } ~
+
+        /**************************************************************************************/
+
+    // Utility Functions
+    char binaryNumber[] = "1001";
+    std::bitset<32>sampleBits(45);
+
+    std::stoi(binaryNumber, 0, 2);              // Convert binary char array to decimal int.
+    sampleBits.count();                         // To count the number on ones binary 45.
+    sampleBits.to_string();                     // To String reresentation of 45 in binary
+    sampleBits.flip();                          // toggles the bit values
+    sampleBits.set(2);                          // sets the selected bit to one and all bits to one if index is not provided
+    sampleBits.reset(2);                        // sets the selected bit to zero and all bits to zero if index is not provided
+
+
+    // To Do Bit Manipulation
+    //https://en.cppreference.com/w/cpp/numeric#Bit_manipulation_.28since_C.2B.2B20.29
+
+    return 0;
+}
--- Utils/Coding Tests/Handy Functions/main.cpp
+++ Utils/Coding Tests/Handy Functions/main.cpp
@@ -5,19 +5,19 @@
 #include <vector>
 #include <numeric> //For accumulate operation
 
-
 int main() {
-
-    // 1. Convert binary to decimal
-
+    // 1. Convert binary to decimal.
     char binaryNumber[] = "1001";
     std::cout << "The decimal representation for binary 1001  is : " << std::stoi(binaryNumber, 0, 2) << std::endl;
 
-    // 2. To count number on ones in a binary number
-    int count = std::bitset<32>(45).count();
+    // 2. To count number on ones in a binary number.
+    std::bitset<32>bitsCountExample(45);
+    int count = bitsCountExample.count();
+    std::string bitString = bitsCountExample.to_string();
     std::cout << "Number of ones in binary 45 is : " << count << std::endl;
+    std::cout << "String reresentation of 45 in binary is : " << bitString << std::endl;
 
-    // 3. STL sort 
+    // 3. STL sort .
     int arr[] = { 3, 5, 1, 2, 4 };
     std::vector<int> holder = { 2,5,9,7,6,4,3 };
     int arSize = sizeof(arr) / sizeof(arr[0]);
@@ -25,18 +25,50 @@ int main() {
     std::sort(holder.begin(), holder.end());    //  2nd way
     std::sort(arr, arr+arSize);                 //  3rd way
 
-    // 4. STL Reverse
+    // 5. Check if char is Alpahabumeric A-Z, a-z, 0-9.
+    bool flag0 = isalnum('.');
+    std::cout << "Check for Alphanumeric A-Z, a-z, 0-9 - flag  :   " << flag0 << std::endl;
+
+    // 5. Check if char is Alpahabumeric A-Z and a-z.
+    bool flag1 = isalpha('a');
+    std::cout << "Check for Alphanumeric A-Z and a-z - flag  :   " << flag1 << std::endl;
+
+    // 4. Check if char is digit
+    bool flag2 = isdigit('6');
+    std::cout << "Check for digit flag  :   " << flag2 << std::endl;
+
+    // 6. Check if char is upper Case A-Z.
+    bool flag3 = isupper('a');
+    std::cout << "Check for Upper Case - flag  :   " << flag3 << std::endl;
+
+    // 7. Convert char to upper case.
+    char temp0 = toupper('a');
+    std::cout << "After Upper case conversion  :   " << temp0 << std::endl;
+
+    to_string()
+    stoi()
+    std::count()
+
+    // 8. Check if char is lower Case a-z.
+    bool flag4 = islower('A');
+    std::cout << "Check for Upper Case - flag  :   " << flag4 << std::endl;
+
+    // 9. Convert char to lower case.
+    char temp1 = tolower('Z');
+    std::cout << "After Upper case conversion  :   " << temp1 << std::endl;
+
+    // 10. STL Reverse
     std::reverse(holder.begin(), holder.end());
 
-    // 5. STL Max
+    // 11. STL Max
     int max = *max_element(holder.begin(), holder.end());
     std::cout << "Max Value : " << max << std::endl;
 
-    // 6. STL Min
+    // 12. STL Min
     int min = *min_element(holder.begin(), holder.end());
     std::cout << "Min Value : " << min << std::endl;
 
-    // 7. STL Accumulate
+    // 13. STL Accumulate
     int accSum = std::accumulate(holder.begin(), holder.end(), 0);
 
 
